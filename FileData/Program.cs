cd51cac4f653a0dcba74ab18953db67e27a765d5
NaRayan <nr.qa2011@gmail.com>
2019-02-14 05:52:51 +0530

    * FileData.sln: Initial draft version
    * Program.cs:
    * ProgramTest.cs:
    * packages.config:
    * FileData.csproj.user:
    * FileDataTest.csproj:

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -7,8 +7,85 @@ namespace FileData
 {
     public static class Program
     {
+        private static FileDetails _fd;
+        /// <summary>
+        /// Gets the fd.
+        /// </summary>
+        /// <value>The fd.</value>
+        public static FileDetails fd
+        {
+            get
+            {
+                if (_fd == null)
+                    return _fd = new FileDetails();
+
+                return _fd;
+            }
+        }
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
         public static void Main(string[] args)
         {
+            if (args.Count() == 0)
+                return;
+
+            foreach (var arg in args)
+            {
+                var items = arg.Split(' ');
+                var result = FileOperation(items[0], items[1]);
+
+                Console.WriteLine(string.Format("Result returned for requested action Type {0} is - {1} ", items[0], result));
+            }
+
+            Console.ReadLine();
+        }
+
+       /// <summary>
+       /// Files the operation.
+       /// </summary>
+       /// <returns>The operation.</returns>
+       /// <param name="action">Action.</param>
+       /// <param name="filePath">File path.</param>
+        public static string FileOperation(string action, string filePath)
+        {
+            var result = string.Empty;
+            string[] possibleVersionArgs = new string[] { "-v", "--v", "/v", "--version" };
+            string[] possibleSizeArgs = new string[] { "-s", "--s", "/s", "--size" };
+
+            if (string.IsNullOrEmpty(action) || string.IsNullOrEmpty(filePath))
+                return string.Empty;
+
+            if (possibleVersionArgs.Contains(action))
+                result = GetFileVersion(filePath);
+
+            if (possibleSizeArgs.Contains(action))
+                result = GetFileSize(filePath).ToString();
+
+            return result;
+        }
+
+       /// <summary>
+       /// Gets the file version.
+       /// </summary>
+       /// <returns>The file version.</returns>
+       /// <param name="filePath">File path.</param>
+        private static string GetFileVersion(string filePath)
+        {
+            return fd.Version(filePath);
+        }
+
+
+        /// <summary>
+        /// Gets the size of the file.
+        /// </summary>
+        /// <returns>The file size.</returns>
+        /// <param name="filePath">File path.</param>
+        private static int GetFileSize(string filePath)
+        {
+            return fd.Size(filePath);
         }
     }
 }
+
---
+++ FileDataTest/ProgramTest.cs
@@ -0,0 +1,169 @@
+﻿using NUnit.Framework;
+using System;
+using FileData;
+using log4net;
+
+namespace FileDataTest
+{
+    /// <summary>
+    /// Program test.
+    /// </summary>
+    [TestFixture()]
+    public class ProgramTest
+    {
+        string filePath = string.Empty;
+        string actionType = string.Empty;
+        /// <summary>
+        /// The logger.
+        /// </summary>
+        public static readonly ILog logger = LogManager.GetLogger("ProgramTest");
+
+        /// <summary>
+        /// Tests the initialization.
+        /// </summary>
+        [TestFixtureSetUp]
+        public void TestInitialization()
+        {
+            filePath = "C:\\test.txt";
+
+        }
+
+        /// <summary>
+        /// Checks for version no on actiontype and filepath test1.
+        /// </summary>
+        /// 
+        /// 
+        [Test()]
+        public void Check_for_version_no_on_actiontype_and_filepath_Test1()
+        {
+            actionType = "-v";
+            var actualResult = Program.FileOperation(actionType, filePath);
+            /// Just written one try catch and logging block in this test, so for best practice can follow for test cases 
+            try
+            {
+                if (!string.IsNullOrEmpty(actualResult))
+                {
+                    Assert.IsTrue(actualResult.Length > 0, string.Format("Version number for {0} passed is - {1}", filePath, actualResult));
+                    logger.Info(string.Format("Version number for {0} passed is - {1}", filePath, actualResult));
+                }
+                else
+                {
+                    logger.Error("Expected result is null or empty" + actualResult);
+                }
+            }
+            catch(Exception ex)
+            {
+                logger.Error("Error: "+ex);
+            }
+
+
+
+        }
+
+
+       /// <summary>
+       /// Checks for version no on actiontype and filepath test2.
+       /// </summary>
+        [Test()]
+        public void Check_for_version_no_on_actiontype_and_filepath_Test2()
+        {
+            actionType = "--v";
+            var actualResult = Program.FileOperation(actionType, filePath);
+
+            if (!string.IsNullOrEmpty(actualResult))
+            {
+                Assert.IsTrue(actualResult.Length > 0, string.Format("Version number for {0} passed is - {1}", filePath, actualResult));
+            }
+        }
+
+        /// <summary>
+        /// Checks for version no on actiontype and filepath test3.
+        /// </summary>
+        [Test()]
+        public void Check_for_version_no_on_actiontype_and_filepath_Test3()
+        {
+            actionType = "/v";
+            var actualResult = Program.FileOperation(actionType, filePath);
+
+            if (!string.IsNullOrEmpty(actualResult))
+            {
+                Assert.IsTrue(actualResult.Length > 0, string.Format("Version number for {0} passed is - {1}", filePath, actualResult));
+            }
+        }
+
+       /// <summary>
+       /// Checks for version no on actiontype and filepath test4.
+       /// </summary>
+        [Test()]
+        public void Check_for_version_no_on_actiontype_and_filepath_Test4()
+        {
+            actionType = "--version";
+            var actualResult = Program.FileOperation(actionType, filePath);
+
+            if (!string.IsNullOrEmpty(actualResult))
+            {
+                Assert.IsTrue(actualResult.Length > 0, string.Format("Version number for {0} passed is - {1}", filePath, actualResult));
+            }
+        }
+
+       /// <summary>
+       /// Checks for filesize on actiontype and filepath test1.
+       /// </summary>
+        [Test()]
+        public void Check_for_filesize_on_actiontype_and_filepath_Test1()
+        {
+            actionType = "-s";
+            var actualResult = Program.FileOperation(actionType, filePath);
+
+            if (!string.IsNullOrEmpty(actualResult))
+            {
+                Assert.IsTrue(Convert.ToInt32(actualResult) > 0, string.Format("filesize for {0} passed is - {1}", filePath, actualResult));
+            }
+        }
+
+        /// <summary>
+        /// Checks for filesize on actiontype and filepath test2.
+        /// </summary>
+        [Test()]
+        public void Check_for_filesize_on_actiontype_and_filepath_Test2()
+        {
+            actionType = "--s";
+            var actualResult = Program.FileOperation(actionType, filePath);
+
+            if (!string.IsNullOrEmpty(actualResult))
+            {
+                Assert.IsTrue(Convert.ToInt32(actualResult) > 0, string.Format("filesize for {0} passed is - {1}", filePath, actualResult));
+            }
+        }
+
+        /// <summary>
+        /// Checks for filesize on actiontype and filepath test3.
+        /// </summary>
+        [Test()]
+        public void Check_for_filesize_on_actiontype_and_filepath_Test3()
+        {
+            actionType = "/s";
+            var actualResult = Program.FileOperation(actionType, filePath);
+
+            if (!string.IsNullOrEmpty(actualResult))
+            {
+                Assert.IsTrue(Convert.ToInt32(actualResult) > 0, string.Format("filesize for {0} passed is - {1}", filePath, actualResult));
+            }
+        }
+
+        /// <summary>
+        /// Checks for filesize on actiontype and filepath test4.
+        /// </summary>
+        [Test()]
+        public void Check_for_filesize_on_actiontype_and_filepath_Test4()
+        {
+            actionType = "--size";
+            var actualResult = Program.FileOperation(actionType, filePath);
+
+            if (!string.IsNullOrEmpty(actualResult))
+            {
+                Assert.IsTrue(Convert.ToInt32(actualResult) > 0, string.Format("filesize for {0} passed is - {1}", filePath, actualResult));
+            }
+        }
+    }
+}
